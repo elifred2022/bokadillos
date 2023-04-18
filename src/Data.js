c57da04a6162dc39a4ab5085b42834a8715e4a19
elifred2022 <elifed21@hotmail.com>
2023-04-18 19:52:17 -0300

    terminado seccion detalles, falta darle forma a inicio y funcionalidad de pago

--- src/Data.js
+++ src/Data.js
@@ -4,9 +4,9 @@ const data = {
     {
       id: 1,
       title: "Docena de Tequeños congelados (precio detal)",
-      price: 780,
+      price: 800,
       image: require("./imagenes/tequeBlack.PNG"),
-      category: "precio venta al menor",
+      category: "congelados",
       img1: "" /* aca se coloca imagen po dafault puede serv cagradas de internet 
                 ej; "https://stockx-360.imgix.net/Nike-Dunk-Low-Off-White-Pine-Green/Images/Nike-Dunk-Low-Off-White-Pine-Green/Lv2/img", 
                 pero en el componente prodcutoItem se coloca en img; <img src={image.default} alt={title} />
@@ -17,9 +17,9 @@ const data = {
     {
       id: 2,
       title: "Docena de Tequeños congelados (precio al mayor)",
-      price: 630,
+      price: 650,
       image: require("./imagenes/tequeHeladera.png"),
-      category: "precio venta al mayor",
+      category: "congelados",
       img1: "https://stockx-360.imgix.net/Nike-Dunk-Low-Off-White-Pine-Green/Images/Nike-Dunk-Low-Off-White-Pine-Green/Lv2/img",
       img2: ".jpg?auto=format,compress&q=90&updated_at=1606319825&w=1000",
       cantidad: 1,
@@ -29,17 +29,29 @@ const data = {
       title: "Docena de Tequeños fritos + rica sala de ajo",
       price: 1500,
       image: require("./imagenes/tequeSalsa.PNG"),
-      category: "docena cocidos",
+      category: "cocidos",
       img1: "https://stockx-360.imgix.net/Nike-Air-Force-1-Low-Supreme-Box-Logo-Black/Images/Nike-Air-Force-1-Low-Supreme-Box-Logo-Black/Lv2/img",
       img2: ".jpg?auto=format,compress&q=90&updated_at=1606325289&w=1000",
       cantidad: 1,
     },
     {
       id: 4,
-      title: "Combo fiestero; Cien 100 Tequeños fritos + rica sala de ajo",
+      title:
+        "Combo fiestero Junior; Cincuanta 50 Tequeños fritos + rica sala de ajo",
+      price: 6250,
+      image: require("./imagenes/tequeMonton.png"),
+      category: "cocidos",
+      img1: "https://stockx-360.imgix.net/Nike-LD-Waffle-Sacai-White-Nylon/Images/Nike-LD-Waffle-Sacai-White-Nylon/Lv2/img",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325338&w=1000",
+      cantidad: 1,
+    },
+    {
+      id: 5,
+      title:
+        "Combo fiestero Sennior; Cien 100 Tequeños fritos + rica sala de ajo",
       price: 12500,
       image: require("./imagenes/tequeMonton.png"),
-      category: "cien cocidos",
+      category: "cocidos",
       img1: "https://stockx-360.imgix.net/Nike-LD-Waffle-Sacai-White-Nylon/Images/Nike-LD-Waffle-Sacai-White-Nylon/Lv2/img",
       img2: ".jpg?auto=format,compress&q=90&updated_at=1606325338&w=1000",
       cantidad: 1,
--- src/componentes/Inicio/index.js
+++ src/componentes/Inicio/index.js
@@ -14,6 +14,38 @@ export const Inicio = () => {
       </Link>
 
       <img src={Picture}></img>
+
+      <div className="description">
+        <p>
+          <b>Que es un Tequeño? </b> Tambien conocido como dedito, palito de
+          queso, rollito de queso o tequeño es una preparación culinaria que
+          consiste en una masa de harina de trigo frita, rellena de queso blanco
+          o feta, entre otros rellenos.
+        </p>
+        <br />
+        <p>
+          Que ingredientes lleva? Harina de trigo, queso blanco, mozzarella,
+          sal, agua.
+        </p>
+        <br />
+        <p>
+          Coccion al horno: Colocamos los tequeños sobre una bandeja de horno
+          con papel o pintada con mantequilla y horneamos a 200ºC durante 17-20
+          minutos hasta que estén doraditos. Si quieres unos tequeños más
+          dorados puedes pincelarlos con huevo. Y una vez fuera del horno.. ¡A
+          comérselos antes de que se enfríen!
+        </p>
+        <br />
+        <p>
+          Coccion fritos: precalentar el aceite previamente a fuego medio alto,
+          cuidando de no quemar el aceite, al momento de freír los tequeños se
+          baja la temperatura a medio, freír los tequeños volteándolos por
+          espacio de 2 a 4 minutos hasta que se doren de manera uniforme. Si el
+          queso comienza a ser visible hay que retirarlos del aceite. Al sacar
+          los tequeños se colocan sobre un recipiente con papel absorbente, se
+          dejan enfrían unos 2 minutos y servir.!
+        </p>
+      </div>
     </div>
   );
 };
